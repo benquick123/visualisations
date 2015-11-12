d64b974bce8ad251f8b5aaf2cf300f3acf68f03a
Tim <tim.oblak1@gmail.com>
2015-11-12 19:17:53 +0100

    odstranjeni komentarji

--- parser.js
+++ parser.js
@@ -17,63 +17,39 @@ $.when(
         tmpData.push(arr);
     })
 ).then(function() {
-    console.log(tmpData);
     data = tmpData;
     masterTable = cloneObject(data[0]);
-    orderMaster();
+    emptyMaster();
     sumTables();
     createTable();
-    /*for (var arr in tmpData) {
-        for (var obj in tmpData[arr]) {
-            //console.log(arr);
-            tmpObj = {};
-            for (var property in tmpData[arr][obj]) {
-                tmpObj[property] = tmpData[arr][obj][property];
-            }
-            if (arr == 0) {
-                data.push(tmpObj);
-            }
-        }
-        //console.log(data);
-    }
-*/
-    //console.log(obj);
+
 });
-function orderMaster(){
+function emptyMaster(){
     for(var prop in masterTable){
         for(var sm in masterTable[prop]) {
             masterTable[prop][sm] = 0;
         }
     }
-
 }
 function sumTables() {
     for (var table in data){
         for(var prop in data[table]){
             for(var sm in data[table][prop]) {
-                if (sm == "id")
-                    masterTable[prop][sm] = parseFloat(data[table][prop][sm]);
-                else
-                    masterTable[prop][sm] += parseFloat(data[table][prop][sm]);
+                if (sm == "id")     masterTable[prop][sm] = parseFloat(data[table][prop][sm]);
+                else                masterTable[prop][sm] += parseFloat(data[table][prop][sm]);
             }
         }
     }
 }
-
 function createTable() {
-    var data0 = masterTable;
-
     var out = "<table>";
-    for (var property in data0[0]) {
+    for (var property in masterTable[0])
         out += "<th>" + property + "</th>";
-    }
 
-    for(var i = 0; i < data0.length; i++) {
+    for(var i = 0; i < masterTable.length; i++) {
         out += "<tr>";
-        for (var property in data0[i]) {
-            out += "<th>" + data0[i][property] + "</th>";
-        }
-
+        for (var property in masterTable[i])
+            out += "<th>" + masterTable[i][property] + "</th>";
         out += "</tr>";
     }
     out += "</table>";
@@ -87,56 +63,3 @@ function cloneObject(obj) {
         temp[key] = cloneObject(obj[key]);
     return temp;
 }
-/*var parser = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var obj = JSON.parse(xmlhttp.responseText);
-        myFunction(obj);
-    }
-};
-
-var ot = new XMLHttpRequest();
-var oi = new XMLHttpRequest();
-var tt = new XMLHttpRequest();
-var ti = new XMLHttpRequest();
-
-ot.onreadystatechange = parser;
-oi.onreadystatechange = parser;
-tt.onreadystatechange = parser;
-ti.onreadystatechange = parser;
-
-ot.open("GET", urls[0], true);
-oi.open("GET", urls[1], true);
-tt.open("GET", urls[2], true);
-ti.open("GET", urls[3], true);
-
-ot.send();
-oi.send();
-tt.send();
-ti.send();
-
-data = [];
-
-function myFunction(arr) {
-    if (data == null)
-        data = arr;
-    else {
-
-
-    }
-    /*data.push(arr);
-    var out = "<table>";
-    for (var property in arr[0]) {
-        out += "<th>" + property + "</th>";
-    }
-
-    for(var i = 0; i < arr.length; i++) {
-        out += "<tr>";
-        for (var property in arr[i]) {
-            out += "<th>" + arr[i][property] + "</th>";
-        }
-
-        out += "</tr>";
-    }
-    out += "</table>";
-    document.getElementById("test").innerHTML = out;*/
-//}
