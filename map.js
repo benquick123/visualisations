def95822cb25f94735e5e01e1acfc683720c93d9
Benjamin <benquick.fele@gmail.com>
2015-11-26 15:47:00 +0100

    update w/ jvectormap

--- map.js
+++ map.js
@@ -20,8 +20,8 @@ function onMouseOutMunicipality() {
 
 function loadMap() {
     var div = d3.select('#map')
-    var svgContainer = div.append('svg').attr("width", window.innerWidth-50).attr("height", window.innerHeight-50);
-    var g = svgContainer.append('g').attr('class', 'svg-container');
+    var svgContainer = div.append('svg').attr("width", window.innerWidth-50).attr("height", window.innerHeight-50).attr("id", "svgParent");
+    var g = svgContainer.append('g').attr('class', 'svg-container').attr('id', 'viewport');
 
     d3.xml("data/obcinemap.svg", "image/svg+xml", function(xml) {
         g.node().appendChild(document.importNode(xml.documentElement, true));
@@ -35,3 +35,47 @@ function loadMap() {
 
     });
 }
+
+//http://paletton.com/#uid=70T1+0kranUh+v7mLrvudjCC4et
+$(function(){
+    var map=$('#world-map').vectorMap({
+        map: 'Obcine_mill_en',
+        backgroundColor: "#e4e4e4",
+        zoomAnimate: true,
+        regionStyle: {
+            initial: {
+                fill: "#888888"
+            },
+            hover: {
+                "fill-opacity": 1,
+                //fill: "#0F3068"
+            }
+        },
+
+        onRegionOver: function(e, code) {
+            var region = $('#world-map').vectorMap('get', 'mapObject').regions[code];
+            var path = d3.select("#" +  code);
+            path.transition().duration(250).style("fill", "#AA2A23");
+        },
+
+        onRegionOut: function(e, code) {
+            var region = $('#world-map').vectorMap('get', 'mapObject').regions[code];
+            var path = d3.select("#" +  code);
+            path.transition().duration(250).style("fill", "#888888");
+        },
+
+        onRegionClick: function (e, code) {
+            var id = code.replace("ob", "");
+            var region = $('#world-map').vectorMap('get', 'mapObject').regions[code];
+            console.log(id);
+            console.log(region);
+        }
+    });
+
+    var svg = $(".jvectormap-container")[0].children[0];
+    var paths = svg.children[1].children;
+    for (var i = 0; i < paths.length; i++) {
+        var dataCode = paths[i].getAttribute("data-code");
+        paths[i].setAttribute("id", dataCode);
+    }
+})
